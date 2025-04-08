73422aa22227a4c6015f8430d17320269becf355
nick <nickchen1998@gmail.com>
2025-04-08 21:14:08 +0800

    功能: 重構跨檔案的 Symptom 模型和視圖
    - 為 `Symptom` 模型新增初始遷移檔案
    - 在 `symptoms/models.py` 中定義 `Symptom` 模型
    - 修改視圖以使用 `symptoms.html` 模板和 `Symptom` 查詢集
    
    Signed-off-by: nick <nickchen1998@gmail.com>

--- symptoms/models.py
+++ symptoms/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Symptom(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    def __str__(self):
+        return self.id
--- symptoms/views.py
+++ symptoms/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.contrib.auth.models import User
+from symptoms.models import Symptom
 
 
 # Create your views here.
 class SymptomListView(ListView):
-    model = User  # Replace with your actual model
-    template_name = 'base.html'  # Replace with your actual template
-    context_object_name = 'symptoms'  # Name of the variable to be used in the template
-    paginate_by = 10  # Number of items per page
+    template_name = 'symptoms.html'
+    context_object_name = 'symptoms'
+    queryset = Symptom.objects.all()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
