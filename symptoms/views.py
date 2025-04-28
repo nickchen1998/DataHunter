fa26dd4a6ac85e970a346dc4bdfa4dbb7c643ad5
nick <nickchen1998@gmail.com>
2025-04-28 20:53:02 +0800

    feat: refactor frontend layout and improve Symptom display
    - Modify CSS styles related to the layout container and sidebar
    - Adjust the height settings for the data table within the sidebar
    - Update the queryset filtering criteria for Symptom objects in views
    - Add pagination to the SymptomListView
    - Expand and collapse content sections in base.html with script buttons
    - Enhance the display of Symptom data in symptoms.html, including handling long text and toggling display
    
    Signed-off-by: nick <nickchen1998@gmail.com>

--- symptoms/views.py
+++ symptoms/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.contrib.auth.models import User
 from symptoms.models import Symptom
+from django.core.paginator import Paginator
 
 
 # Create your views here.
 class SymptomListView(ListView):
     template_name = 'symptoms.html'
     context_object_name = 'symptoms'
-    queryset = Symptom.objects.all()
+    queryset = Symptom.objects.order_by("-question_time").all()
+    paginate_by = 10
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Add any additional context variables here
+        # print(context.values())
+
         return context
