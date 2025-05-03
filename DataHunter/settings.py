9fc1572b7b4cdf49cfea100912dc6a9ba1aa3eaf
nickchen <nickchen@eland.com.tw>
2025-05-04 01:05:51 +0800

    GIT 提交訊息：
    feat: 重構資料庫配置並改善數據顯示邏輯
    
    - 根據 `USE_SQLITE` 環境變數引入條件資料庫配置。
    - 修改 HTML 模板中的按鈕結構，以改善數據顯示區域的格式。
    - 加強 HTML 模板中數據呈現的條件顯示邏輯。
    
    Signed-off-by: nickchen <nickchen@eland.com.tw>

--- DataHunter/settings.py
+++ DataHunter/settings.py
@@ -73,17 +73,24 @@ WSGI_APPLICATION = 'DataHunter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "DataHunter",
-        'USER': "root",
-        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
-        'HOST': os.getenv("POSTGRES_HOST", "localhost"),
-        'PORT': "5432",
+if os.getenv("USE_SQLITE", "False") == "True":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': "DataHunter",
+            'USER': "root",
+            'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+            'HOST': os.getenv("POSTGRES_HOST", "localhost"),
+            'PORT': "5432",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
